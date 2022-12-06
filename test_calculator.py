0198d7ef79b092c8ea27970b9cc3365a0f8d5269
Matt Carr <mhcarr17@gmail.com>
2022-12-06 08:04:29 -0600

    reformat to pep8 standards

--- test_calculator.py
+++ test_calculator.py
@@ -6,8 +6,10 @@ import calculator
 
 class TestCalculator:
 
+
     def test_addition(self):
-        assert calculator.add(2,2) == 4
+        assert calculator.add(2, 2) == 4
+
 
     def test_subtraction(self):
-        assert calculator.subtract(4,2) == 2
+        assert calculator.subtract(4, 2) == 2
