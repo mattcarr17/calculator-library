0139699c4ac1ff8159c1c030064b9ff48bfec6b6
Matt Carr <mhcarr17@gmail.com>
2022-12-06 07:48:02 -0600

    add initial functionality for calculator

---
+++ test_calculator.py
@@ -0,0 +1,13 @@
+'''
+Unit tests for the calculator library
+'''
+
+import calculator
+
+class TestCalculator:
+
+    def test_addition(self):
+        assert calculator.add(2,2) == 4
+
+    def test_subtraction(self):
+        assert calculator.subtract(4,2) == 2
